b519b381eb0c446b4fdf14107336db3c9a55ea98
M ARBAB ANJUM <arbabanjum0347@gmail.com>
2020-12-23 21:54:10 +0500

    let and const

---
+++ 1 let and const/script.js
@@ -0,0 +1,31 @@
+// let name = "arbab";
+// if (true) {
+//     name = "arain";
+// }
+// console.log(name);
+///////////////////////////////////////
+
+// let name;
+// function fname() {
+//     name = "arbab";
+// }
+// fname();
+// console.log(name)
+//////////////////////////////////////
+
+// var name = "arain";
+// function fname() {
+//     var name = "arbab";
+// }
+// fname();
+// console.log(name)
+//////////////////////////////////////
+
+const name = "Arbab"
+// name = "error"
+// console.log(name)
+
+var obj = {
+    firstname: name
+}
+console.log(obj)
