25a41653c4846577995de221fcac9bf2a25eaf61
M ARBAB ANJUM <arbabanjum0347@gmail.com>
2020-12-23 22:25:19 +0500

    Literals and Spread operators

---
+++ 2 string Literals/script.js
@@ -0,0 +1,3 @@
+let name = "arbab";
+let age = "21";
+document.write(`My name is ${name} and my age is ${age}`)
---
+++ 3 Spread Operators/script.js
@@ -0,0 +1,11 @@
+var arr1 = ["a", "b", "c", "d"]
+var arr2 = [...arr1, "e", "f", "g", "h"]
+
+
+//Simple JS
+// var arr3 = arr1.concat(arr2);
+// console.log(arr3)
+
+
+//using spread Operator
+console.log(arr2);
